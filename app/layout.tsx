dcdb88cfec435ae24677c8af64c9014233e21ee6
Viktor Riabkov <vitya.ryabkov@gmail.com>
2025-10-31 13:11:49 +0400

    Update the metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import { SessionProvider } from "next-auth/react";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://chat.vercel.ai"),
-  title: "Next.js Chatbot Template",
-  description: "Next.js chatbot template using the AI SDK.",
+  title: "GIPITI",
+  description: "Gipiti is a platform for creating and sharing AI-powered tools.",
 };
 
 export const viewport = {
