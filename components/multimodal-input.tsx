31f74e2ee81312580fff866bc942139b3e0fca9e
Viktor Riabkov <vitya.ryabkov@gmail.com>
2025-11-20 14:01:01 +0400

    Change reset password to save new password label and added stop button for llm streaming

--- components/multimodal-input.tsx
+++ components/multimodal-input.tsx
@@ -331,7 +331,7 @@ function PureMultimodalInput({
             />
           </PromptInputTools>
 
-          {status === "submitted" ? (
+          {status === "submitted" || status === "streaming" ? (
             <StopButton setMessages={setMessages} stop={stop} />
           ) : (
             <PromptInputSubmit
